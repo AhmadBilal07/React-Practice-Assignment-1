cca753ae478c98802cb469de838f96e4707a3e4a
AhmadBilal07 <ahmadbilal07@live.com>
2020-10-09 01:48:36 +0200

    initial code setup

---
+++ react_assign_1/src/UserOutput/UserOutput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const userOutput = () => {
+
+return (
+    <p>lorem ipsumaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa </p>
+    )
+
+
+};
+export default userOutput;
