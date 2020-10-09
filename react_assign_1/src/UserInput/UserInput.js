7a9d1ac718ebf66810e2a9eade3c5654cf9cf96d
AhmadBilal07 <ahmadbilal07@live.com>
2020-10-10 01:59:02 +0200

    input firld styling update

--- react_assign_1/src/UserInput/UserInput.js
+++ react_assign_1/src/UserInput/UserInput.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const userInput = (props) => {
 const style = {
-    border:"2px solid red"
+    border:"2px solid red",
+    "margin": "20px"
 }
 return (
     <input type="text" style={style} onChange = {props.Change} value={props.userName}/>
